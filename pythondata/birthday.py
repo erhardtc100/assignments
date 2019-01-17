b42050aa12e2cf98d1f1f425288c09f09966f207
Colin Erhardt <erhardtc100@gmail.com>
2019-01-17 14:35:36 -0500

    Add date converter

---
+++ pythondata/birthday.py
@@ -0,0 +1,11 @@
+#Colin and Dana
+from datetime import datetime
+
+birthday = "1-May-12"
+date_format = "%d-%b-%y"
+
+parsed_date = datetime.strptime(birthday, date_format)
+date_str = parsed_date.strftime("%-m/%-d/%Y") # 5/1/2012
+
+print(date_str)
+
